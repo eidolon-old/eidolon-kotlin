404203b0576b602526709f82abe9fb3ab0671c01
Elliot Wright <wright.elliot@gmail.com>
2015-06-13 16:42:36 +0100

    Multi-project build.

---
+++ eidolon-http-foundation/src/main/kotlin/space/eidolon/httpfoundation/Greeter.kt
@@ -0,0 +1,21 @@
+/**
+ * This file is part of the "eidolon" project.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the LICENSE is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package space.eidolon.httpfoundation
+
+/**
+ * Greeter
+ *
+ * @author Elliot Wright <elliot@elliotwright.co>
+ */
+public class Greeter {
+    val greeting = "Hello world"
+}
